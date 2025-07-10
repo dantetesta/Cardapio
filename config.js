5ed78a7b451414b3ce60cc92d47e89865c4f4e2b
DANTE TESTA <suporte@dantetesta.com.br>
2025-07-09 22:58:43 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+const CONFIG_PLANILHAS = {
+    categorias: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vST_145cenMkoqUUiTNF00kuNjECQe0WUwa4zVL-qrDcoNfIhCmo3xXXVrgIVj65VpQEZv85Eetoqnd/pub?gid=244484282&single=true&output=csv',
+    itens: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vST_145cenMkoqUUiTNF00kuNjECQe0WUwa4zVL-qrDcoNfIhCmo3xXXVrgIVj65VpQEZv85Eetoqnd/pub?gid=691800124&single=true&output=csv',
+    config: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vST_145cenMkoqUUiTNF00kuNjECQe0WUwa4zVL-qrDcoNfIhCmo3xXXVrgIVj65VpQEZv85Eetoqnd/pub?gid=0&single=true&output=csv'
+};
